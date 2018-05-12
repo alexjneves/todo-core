29fff52423df30b142cc026a0dee4e86e09e38fa
Alex Neves <alexneves08@gmail.com>
2018-05-12 23:37:18 +0100

    TodoList component

---
+++ TodoCore/ClientApp/components/todo/TodoList.tsx
@@ -0,0 +1,19 @@
+﻿import * as React from 'react';
+import { RouteComponentProps } from 'react-router';
+import 'isomorphic-fetch';
+
+interface TodoListState {
+}
+
+export class TodoList extends React.Component<RouteComponentProps<{}>, TodoListState> {
+    constructor() {
+        super();
+        this.state = {};
+    }
+
+    public render() {
+        return <div>
+                   <h1>To-do List</h1>
+               </div>;
+    }
+}
